e30509a35ebfaa1ae48a6da9b20d069d0e7a5d16
Jaime Mas Sanntillán <38068010+TheArmega@users.noreply.github.com>
2021-09-23 12:32:22 +0200

    Delete catkin_lola2/build directory

--- catkin_lola2/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gram/catkin_lola2/src
-# Build directory: /home/gram/catkin_lola2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("lola2_global")
--- catkin_lola2/build/lola2_global/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gram/catkin_lola2/src/lola2_global
-# Build directory: /home/gram/catkin_lola2/build/lola2_global
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
